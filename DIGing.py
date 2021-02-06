b104e7fbea88ec98d4eaa47444685cc2148ad8e7
gaohuiguo88 <gaohuiguo88@163.com>
2021-02-06 15:51:34 +0800

    Make some changes to the DIGing.py

--- DIGing.py
+++ DIGing.py
@@ -1,41 +1,63 @@
 import torch
 import math
 import copy
-# problem:
 L = 10
-# noise = math.sqrt(0.01)*torch.randn((L,3))
 noise = math.sqrt(0)*torch.randn((L,3))
 U = torch.randn((L,3,3))
 x = 0.9*torch.ones((L,3))
-
 v = torch.empty((L,3))
 for i in range(L):
     v[i] = torch.matmul(U[i],x[i])+noise[i]
 
-Iteration = 10000
+Iteration = 10
 W = 1/L * torch.ones((L,L))
 recon_x = torch.zeros((L,3),requires_grad=True)
 store_grad = torch.zeros((L,3))
 y = torch.zeros((L,3))
+alpha = 1
+loss = torch.norm(recon_x - x)
+print("loos_x: ",loss.data)
+# Initialization y
+for i in range(L):
+    formula = v[i] - torch.matmul(U[i], recon_x[i])
+    f = torch.matmul(torch.transpose(formula.unsqueeze(1), 0, 1), formula.unsqueeze(1))
+    f.backward()
+
+with torch.no_grad():
+    for i in range(L):
+        y[i] = recon_x.grad[i]
+        store_grad[i] = recon_x.grad[i]
+
+    cp_recon_x = copy.deepcopy(recon_x)
+    for i in range(L):
+        recon_x[i] = torch.matmul(W[i],cp_recon_x) - alpha * y[i]
+
+loss = torch.norm(recon_x - x)
+print("loos_x: ",loss.data)
+
 for k in range(Iteration):
-    alpha = 0.1/(k+1)
     for i in range(L):
         formula = v[i]-torch.matmul(U[i],recon_x[i])
         f = torch.matmul(torch.transpose(formula.unsqueeze(1), 0, 1), formula.unsqueeze(1))
         f.backward()
         # print(recon_x)
-        loss = torch.norm(recon_x - x)
-        # print(loss.data)
-    if k == 0:
+
+    with torch.no_grad():
+        cp_recon_x = copy.deepcopy(recon_x)
+        cp_y = copy.deepcopy(y)
+
         for i in range(L):
-            y[i] = recon_x.grad[i]
-    cp_recon_x = copy.deepcopy(recon_x)
-    for i in range(L):
-        with torch.no_grad():
-            # print(recon_x.grad[i])
-            recon_x[i] = torch.matmul(W[i],cp_recon_x) - alpha * y[i]
-            y[i] = torch.matmul(W[i],y) + recon_x.grad[i] - store_grad[i]
+            y[i] = torch.matmul(W[i], cp_y) + recon_x.grad[i] - store_grad[i]
             store_grad[i] = recon_x.grad[i]
+
+        loss_y = torch.norm(recon_x.grad-y)
+        print("loss_y: ",loss_y.data)
+
+        for i in range(L):
+            recon_x[i] = torch.matmul(W[i],cp_recon_x) - alpha * y[i]
+
+        loss = torch.norm(recon_x - x)
+        print("loos_x: ",loss.data)
 
 for i in range(L):
     print(recon_x[i])
