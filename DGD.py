87d028717c92ffac954d4b4fb2cfc43ab0a08787
gaohuiguo88 <gaohuiguo88@163.com>
2021-02-21 14:22:44 +0800

    update the step size of DGD

--- DGD.py
+++ DGD.py
@@ -1,6 +1,7 @@
 import torch
 import math
 import copy
+import matplotlib.pyplot as plt
 torch.manual_seed(0)
 # problem:
 L = 10
@@ -13,30 +14,39 @@ v = torch.empty((L,3))
 for i in range(L):
     v[i] = torch.matmul(U[i],x[i])+noise[i]
 
-Iteration = 100
+Iteration = 10000
 W = 1/L * torch.ones((L,L))
 recon_x = torch.zeros((L,3),requires_grad=True)
+loss_list = []
 
 for k in range(Iteration):
-    alpha = 0.1/(k+1)
+    alpha = 6e-2
     for i in range(L):
         formula = v[i]-torch.matmul(U[i],recon_x[i])
         f = torch.matmul(torch.transpose(formula.unsqueeze(1), 0, 1), formula.unsqueeze(1))
         f.backward()
         # print(recon_x)
-        loss = torch.norm(recon_x - x)
+    loss = torch.norm(recon_x - x)
+    loss_list.append(loss.data)
         # print(loss.data)
     cp_recon_x = copy.deepcopy(recon_x)
     for i in range(L):
         with torch.no_grad():
             # print(recon_x.grad[i])
             recon_x[i] = torch.matmul(W[i],cp_recon_x) - alpha * recon_x.grad[i]
+
+
     # if k == 0:
     #     for i in range(L):
     #         # print(recon_x.grad[i])
     #         print(recon_x[i])
 
-for i in range(L):
-    print(recon_x[i])
+# for i in range(L):
+#     print(recon_x[i])
+
+x_label = [i for i in range(len(loss_list))]
+plt.plot(x_label,loss_list)
+plt.show()
+print(loss_list[len(loss_list)-1])
 
 
