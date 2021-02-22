0ff68f5c9fac5e9c24b6a8e22d724c0b73dbd269
gaohuiguo88 <gaohuiguo88@163.com>
2021-02-22 15:56:43 +0800

    update the network spectrum and weight generation function of DGD

--- DGD.py
+++ DGD.py
@@ -2,6 +2,44 @@ import torch
 import math
 import copy
 import matplotlib.pyplot as plt
+import scipy.sparse as sp
+from torch_geometric.data import Data
+import numpy as np
+# create graph
+# problem:
+L = 12
+node_feature = 3
+# edge_index = torch.tensor([[0, 0, 1, 2, 3, 3, 4, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 10, 11, 11, 11],
+#                            [9, 5, 9, 11, 10, 8, 2, 2, 7, 3, 5, 11, 0, 9, 5, 2, 4, 10, 6, 4, 0, 1, 4, 5]], dtype=torch.long)
+edge_index = torch.tensor([[0, 0, 0, 0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 7, 8],
+                           [5, 7, 9, 10, 9, 11, 4, 5, 8, 11, 6, 8, 10, 8, 9, 11, 6, 7, 11, 9, 11, 9, 10]], dtype=torch.long)
+edge_index = torch.cat((edge_index,torch.cat((edge_index[1],edge_index[0])).reshape(2,-1)),1)
+print(edge_index)
+x = 300*torch.ones((L,node_feature))
+
+data = Data(x=x, edge_index=edge_index)
+
+
+def generate_Metropolis_W(data):
+    row = data.edge_index[0].cpu()
+    col = data.edge_index[1].cpu()
+    raw_data = torch.ones(data.edge_index.shape[1])
+    adj = torch.Tensor(sp.coo_matrix((raw_data, (row, col)), shape=(data.x.shape[0], data.x.shape[0])).toarray())
+    # adj = torch.Tensor(sp.coo_matrix((raw_data, (row, col)), shape=(L, L)).toarray())
+    degree = torch.sum(adj, 1)
+
+    W = torch.zeros((data.x.shape[0], data.x.shape[0]))
+    for i in range(data.edge_index.shape[1]):
+        W[data.edge_index[0][i]][data.edge_index[1][i]] = 1 / (
+                    2 * max(degree[data.edge_index[0][i]], degree[data.edge_index[1][i]]))
+    for i in range(data.x.shape[0]):
+        sum_W = 0
+        for j in range(data.x.shape[0]):
+            if adj[i][j] != 0:
+                sum_W += W[i][j]
+        W[i][i] = 1 - sum_W
+
+    return W
 
 def norm_f(x):
     m,n = x.shape
@@ -13,20 +51,21 @@ def norm_f(x):
     return total_sum
 
 torch.manual_seed(0)
-# problem:
-L = 10
-# noise = math.sqrt(0.01)*torch.randn((L,3))
-noise = math.sqrt(0)*torch.randn((L,3))
-U = torch.randn((L,3,3))
-x = 300*torch.ones((L,3))
+
+# noise = math.sqrt(0.01)*torch.randn((L,node_feature))
+noise = math.sqrt(0)*torch.randn((L,node_feature))
+U = torch.randn((L,node_feature,node_feature))
+# x = 300*torch.ones((L,node_feature))
 # print(U)
-v = torch.empty((L,3))
+v = torch.empty((L,node_feature))
 for i in range(L):
     v[i] = torch.matmul(U[i],x[i])+noise[i]
 
 Iteration = 2500
-W = 1/L * torch.ones((L,L))
-recon_x = torch.zeros((L,3),requires_grad=True)
+# W = 1/L * torch.ones((L,L))
+W = generate_Metropolis_W(data)
+print(W)
+recon_x = torch.zeros((L,node_feature),requires_grad=True)
 loss_list = []
 
 for k in range(Iteration):
@@ -36,7 +75,7 @@ for k in range(Iteration):
         f = torch.matmul(torch.transpose(formula.unsqueeze(1), 0, 1), formula.unsqueeze(1))
         f.backward()
         # print(recon_x)
-    loss = norm_f(recon_x - x)/norm_f(torch.zeros((L,3))-x)
+    loss = norm_f(recon_x - x)/norm_f(torch.zeros((L,node_feature))-x)
     loss_list.append(loss.data)
     print("Iteration ", k, " : ",loss.data)
     cp_recon_x = copy.deepcopy(recon_x)
@@ -59,5 +98,9 @@ plt.plot(x_label,loss_list)
 plt.yscale('log')
 plt.show()
 print(loss_list[len(loss_list)-1])
+
+
+
+
 
 
