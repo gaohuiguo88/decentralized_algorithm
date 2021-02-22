ecbec28d0fdf09fe4dfa626cba08247cdd64ed49
gaohuiguo88 <gaohuiguo88@163.com>
2021-02-22 13:36:33 +0800

    update the plot figure of DGD

--- DGD.py
+++ DGD.py
@@ -2,19 +2,29 @@ import torch
 import math
 import copy
 import matplotlib.pyplot as plt
+
+def norm_f(x):
+    m,n = x.shape
+    x = x.reshape(m*n)
+    total_sum = 0
+    for i in range(m*n):
+        total_sum += x[i]*x[i]
+    total_sum = torch.sqrt(total_sum)
+    return total_sum
+
 torch.manual_seed(0)
 # problem:
 L = 10
 # noise = math.sqrt(0.01)*torch.randn((L,3))
 noise = math.sqrt(0)*torch.randn((L,3))
 U = torch.randn((L,3,3))
-x = torch.ones((L,3))
+x = 300*torch.ones((L,3))
 # print(U)
 v = torch.empty((L,3))
 for i in range(L):
     v[i] = torch.matmul(U[i],x[i])+noise[i]
 
-Iteration = 10000
+Iteration = 2500
 W = 1/L * torch.ones((L,L))
 recon_x = torch.zeros((L,3),requires_grad=True)
 loss_list = []
@@ -26,9 +36,9 @@ for k in range(Iteration):
         f = torch.matmul(torch.transpose(formula.unsqueeze(1), 0, 1), formula.unsqueeze(1))
         f.backward()
         # print(recon_x)
-    loss = torch.norm(recon_x - x)
+    loss = norm_f(recon_x - x)/norm_f(torch.zeros((L,3))-x)
     loss_list.append(loss.data)
-        # print(loss.data)
+    print("Iteration ", k, " : ",loss.data)
     cp_recon_x = copy.deepcopy(recon_x)
     for i in range(L):
         with torch.no_grad():
@@ -43,9 +53,10 @@ for k in range(Iteration):
 
 # for i in range(L):
 #     print(recon_x[i])
-
+print(len(loss_list))
 x_label = [i for i in range(len(loss_list))]
 plt.plot(x_label,loss_list)
+plt.yscale('log')
 plt.show()
 print(loss_list[len(loss_list)-1])
 
